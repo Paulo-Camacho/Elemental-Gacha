fdaef29271aa1f792100d55b2713a9738ffcd151
Andrew-Ryer <aryer@csumb.edu>
2025-11-12 14:16:02 -0800

    Admin/PullRate files

---
+++ app/src/main/java/com/example/mydemoapp/AdminEditPullRateActivity.java
@@ -0,0 +1,16 @@
+package com.example.mydemoapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminEditPullRateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_admin_edit_pull_rate);
+
+    }
+}
