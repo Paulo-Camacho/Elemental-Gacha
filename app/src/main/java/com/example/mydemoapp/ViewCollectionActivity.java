91009bae64c58124503fc7250016e181bcda3861
nkchelonis <nkchelonis@gmail.com>
2025-11-12 15:24:37 -0800

    Added intent factories buttons should theoretically swap activities

--- app/src/main/java/com/example/mydemoapp/ViewCollectionActivity.java
+++ app/src/main/java/com/example/mydemoapp/ViewCollectionActivity.java
@@ -1,6 +1,10 @@
 package com.example.mydemoapp;
 
+import android.content.ContentProvider;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.mydemoapp.databinding.ActivityViewCollectionBinding;
+
+/**
+ * Collection View
+ * Users can: see collection, go back
+ * @author Nat Chelonis
+ * @since 3 - Nov - 2025
+ */
 public class ViewCollectionActivity extends AppCompatActivity {
+
+    private ActivityViewCollectionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_view_collection);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = ActivityViewCollectionBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //TODO: make this send user back to correct landing page
+        binding.collectionBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = PremiumUserLandingPageActivity.premiumUserIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
         });
+
+
+    }
+
+    static Intent viewCollectionIntentFactory(Context context){
+        return new Intent(context, ViewCollectionActivity.class);
     }
 }
