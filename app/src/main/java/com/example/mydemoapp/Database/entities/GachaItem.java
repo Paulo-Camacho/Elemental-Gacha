301e36b6a7814789f82b4df47b08edb856538916
Paulo-Camacho <anthropologyparliament@gmail.com>
2025-11-12 20:09:21 -0800

    Working on GachaItem

---
+++ app/src/main/java/com/example/mydemoapp/Database/entities/GachaItem.java
@@ -0,0 +1,61 @@
+package com.example.mydemoapp.Database.entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.example.mydemoapp.Database.GachaDatabase;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity(tableName = GachaDatabase.GACHA_TABLE)
+public class GachaItem {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String itemName;
+    private String rarity;
+    private LocalDate datePulled;
+
+    public GachaItem(String itemName, String rarity) {
+        this.itemName = itemName;
+        this.rarity = rarity;
+        this.datePulled = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "GachaItem{" +
+                "id=" + id +
+                ", itemName='" + itemName + '\'' +
+                ", rarity='" + rarity + '\'' +
+                ", datePulled=" + datePulled +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GachaItem)) return false;
+        GachaItem that = (GachaItem) o;
+        return id == that.id && Objects.equals(itemName, that.itemName) && Objects.equals(rarity, that.rarity) && Objects.equals(datePulled, that.datePulled);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, itemName, rarity, datePulled);
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getItemName() { return itemName; }
+    public void setItemName(String itemName) { this.itemName = itemName; }
+
+    public String getRarity() { return rarity; }
+    public void setRarity(String rarity) { this.rarity = rarity; }
+
+    public LocalDate getDatePulled() { return datePulled; }
+    public void setDatePulled(LocalDate datePulled) { this.datePulled = datePulled; }
+}
