0f66dfe03071a619a7c5460cb7e32922901fc358
nkchelonis <nkchelonis@gmail.com>
2025-12-03 14:09:53 -0800

    I have implemented a logout dialog window when you click the logout button. (It works!)

--- app/src/main/java/com/example/mydemoapp/PremiumUserLandingPageActivity.java
+++ app/src/main/java/com/example/mydemoapp/PremiumUserLandingPageActivity.java
@@ -1,12 +1,14 @@
 package com.example.mydemoapp;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 
@@ -65,9 +67,7 @@ public class PremiumUserLandingPageActivity extends AppCompatActivity {
         binding.premiumUserLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                logout();
-                Intent intent = MainActivity.mainActivityFactory(getApplicationContext());
-                startActivity(intent);
+                showLogoutDialog();
             }
         });
 
@@ -117,14 +117,44 @@ public class PremiumUserLandingPageActivity extends AppCompatActivity {
     }
 
     private void logout() {
-        loggedInUserID = LOGGED_OUT;
-        updateSharedPreference();
-        getIntent().putExtra(PREMIUM_USER_ACTIVITY_USER_ID,LOGGED_OUT);
-        startActivity((LoginActivity.loginIntentFactory(getApplicationContext())));
+        // Clear SharedPreferences
+        SharedPreferences sharedPreferences = getApplicationContext()
+                .getSharedPreferences(getString(R.string.preference_file_key), MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt(getString(R.string.preference_userId_key), LOGGED_OUT);
+        editor.apply();
+
+        // Go to MainActivity
+        Intent intent = MainActivity.mainActivityFactory(getApplicationContext());
+        startActivity(intent);
+        // prevents back button from returning here
+        finish();
     }
 
     private void toastMaker(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void showLogoutDialog() {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(PremiumUserLandingPageActivity.this);
+        final AlertDialog alertDialog = alertBuilder.create();
+        alertBuilder.setMessage("Would you like to logout?");
+        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                logout();
+            }
+        });
+
+        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                alertDialog.dismiss();
+            }
+        });
+
+        alertBuilder.create().show();
     }
 
 }
